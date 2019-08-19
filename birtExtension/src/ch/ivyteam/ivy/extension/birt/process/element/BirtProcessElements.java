0015f0dd7486818e408c33f96555a2a86d257f27
Alexander Suter <alexander.suter@axonivy.com>
2019-08-19 14:14:36 +0200

    XIVY-3281 Fix sonar findings

--- birtExtension/src/ch/ivyteam/ivy/extension/birt/process/element/BirtProcessElements.java
+++ birtExtension/src/ch/ivyteam/ivy/extension/birt/process/element/BirtProcessElements.java
@@ -10,9 +10,6 @@ import ch.ivyteam.ivy.process.element.IExtensibleProcessElementFactory;
 import ch.ivyteam.ivy.process.element.IExtensibleStandardProcessElementExtension;
 
 /**
- * Xpert.ivy extensible standard process element extension for SAP process
- * elements
- * 
  * @author mde
  * @since 27.01.2009
  */
@@ -29,31 +26,30 @@ public class BirtProcessElements implements IExtensibleStandardProcessElementExt
   {
     if (CreateReportDialog.ICON.equals(iconName) || CreateReportXmlDialog.ICON.equals(iconName))
     {
-      URL iconUrl = getClass().getClassLoader().getResource(CreateReportDialog.ICON + ".png");
-      if (iconUrl != null)
-      {
-        return iconFactory.createActivityBpmnIcon(new ImageIcon(iconUrl), false);
-      }
+      return createActivityBpmnIcon(iconFactory, CreateReportDialog.ICON);
     }
-    else if (iconName.equals(CreateDynamicReportDialog.ICON))
+    else if (CreateDynamicReportDialog.ICON.equals(iconName))
     {
-      URL iconUrl = getClass().getClassLoader().getResource(CreateDynamicReportDialog.ICON + ".png");
-      if (iconUrl != null)
-      {
-        return iconFactory.createActivityBpmnIcon(new ImageIcon(iconUrl), false);
-      }
+      return createActivityBpmnIcon(iconFactory, CreateDynamicReportDialog.ICON);
     }
     return null;
   }
 
+  private Icon createActivityBpmnIcon(IBpmnIconFactory iconFactory, String icon)
+  {
+    URL iconUrl = getClass().getClassLoader().getResource(icon + ".png");
+    if (iconUrl == null)
+    {
+      return null;
+    }
+    return iconFactory.createActivityBpmnIcon(new ImageIcon(iconUrl), false);
+  }
+  
   @Override
   public void declareProcessElements(IExtensibleProcessElementFactory factory)
   {
-    factory.declareProgramInterfaceProcessElement(CreateReportDialog.ID,
-            CreateReportDialog.class.getName(), CreateReportDialog.ICON);
-    factory.declareProgramInterfaceProcessElement(CreateReportXmlDialog.ID,
-            CreateReportXmlDialog.class.getName(), CreateReportXmlDialog.ICON);
-    factory.declareProgramInterfaceProcessElement(CreateDynamicReportDialog.ID,
-            CreateDynamicReportDialog.class.getName(), CreateDynamicReportDialog.ICON);
+    factory.declareProgramInterfaceProcessElement(CreateReportDialog.ID, CreateReportDialog.class.getName(), CreateReportDialog.ICON);
+    factory.declareProgramInterfaceProcessElement(CreateReportXmlDialog.ID, CreateReportXmlDialog.class.getName(), CreateReportXmlDialog.ICON);
+    factory.declareProgramInterfaceProcessElement(CreateDynamicReportDialog.ID, CreateDynamicReportDialog.class.getName(), CreateDynamicReportDialog.ICON);
   }
 }
